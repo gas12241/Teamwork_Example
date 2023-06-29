d3a09320a4a942a7ecbdff90811c0f43af95ec60
gas12241 <georgepitcher11@gmail.com>
2023-06-29 16:11:37 -0700

    more added to quote

--- src/main/java/com/company/TeamworkExample/models/Quote.java
+++ src/main/java/com/company/TeamworkExample/models/Quote.java
@@ -3,5 +3,9 @@ package com.company.TeamworkExample.models;
 public class Quote {
     private Integer id;
     private String text;
+    private String hai;
+    private String helloo;
+    private String hey;
+
 
 }
